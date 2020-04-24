a24829a9098499ff2ae6b271ac07a0fa82a9b96d
thystler <thystler@gmail.com>
2020-04-24 14:55:38 +0200

    automatic enterJar changes

--- FOC/src/main/java/nl/tudelft/trustchain/FOC/MainActivityFOC.kt
+++ FOC/src/main/java/nl/tudelft/trustchain/FOC/MainActivityFOC.kt
@@ -177,6 +177,17 @@ class MainActivityFOC : AppCompatActivity() {
             // magnetLink = "magnet:?xt=urn:btih:dd8255ecdc7ca55fb0bbf81323d87062db1f6d1c&dn=Big+Buck+Bunny&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fbig-buck-bunny.torrent";
         } else magnetLink = inputText
 
+        val startindexname = magnetLink.indexOf("&dn=")
+
+        val stopindexname = if (magnetLink.contains("&dn="))  magnetLink.length else magnetLink.indexOf("&tr")
+
+
+        val magnetnameraw = magnetLink.substring(startindexname + 4,stopindexname)
+        Log.i("personal", magnetnameraw)
+        val magnetname = magnetnameraw.replace('+',' ',false)
+        Log.i("personal",magnetname)
+        enterJar.setText(magnetname)
+
         val sp = SettingsPack()
         sp.seedingOutgoingConnections(true)
         val params =
